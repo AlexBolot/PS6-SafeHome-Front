a8a55122159be791287f20b3b2ccd74fea2fbb2c
Mathieu Paillart <mathieu.paillart@etu.unice.fr>
2018-03-17 18:29:56 +0100

    SHB-25 popup added + link between issueForm and issueView added

---
+++ smart-home-b/src/app/popupissue/popupissue.component.ts
@@ -0,0 +1,25 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {Router} from '@angular/router';
+
+  @Component({
+    selector: 'app-popupissue',
+    templateUrl: './popupissue.component.html',
+    styleUrls: ['./popupissue.component.css']
+  })
+  export class PopupissueComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<PopupissueComponent>,
+    public router: Router,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+    this.router.navigate(['/issueView']);
+  }
+  onOkClick(): void {
+    this.dialogRef.close();
+    this.router.navigate(['/issueForm']);
+  }
+}
