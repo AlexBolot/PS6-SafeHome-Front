32f3482827d4327eefb0cb049d7dc564bd182663
Gregoire Peltier <pg501842@etu.unice.fr>
2018-03-20 10:20:23 +0100

    SHB-33 Changed the needed lines to use the API_ROOT resource

--- smart-home-b/src/app/service/issue/issue.service.ts
+++ smart-home-b/src/app/service/issue/issue.service.ts
@@ -2,11 +2,12 @@ import {Injectable} from '@angular/core';
 import {Issue} from '../../model/issue';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
+import {AppSettings} from "../../model/app-settings";
 
 @Injectable()
 export class IssueService {
 
-  API_url = 'https://galles.io/pns/shb/api/Issues';
+  API_url = AppSettings.API_ROOT+'/api/Issues';
 
   constructor(private httpClient: HttpClient) {
   }
--- smart-home-b/src/app/service/urgency/urgency.service.ts
+++ smart-home-b/src/app/service/urgency/urgency.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
+import {AppSettings} from "../../model/app-settings";
 
 @Injectable()
 export class UrgencyService {
 
-  API_url = 'https://galles.io/pns/shb/api/Urgencies';
+  API_url = AppSettings.API_ROOT+'/Urgencies';
 
   constructor(private httpClient: HttpClient) {
   }
