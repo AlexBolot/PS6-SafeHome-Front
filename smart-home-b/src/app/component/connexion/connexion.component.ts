58e0d78a55efdd25bc6579d6afb8b8eaeee5fc11
Mathieu Paillart <mathieu.paillart@etu.unice.fr>
2018-03-22 21:24:06 +0100

    SHB-39 Animation connexion added

--- smart-home-b/src/app/component/connexion/connexion.component.ts
+++ smart-home-b/src/app/component/connexion/connexion.component.ts
@@ -1,29 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { Router} from "@angular/router";
-import {AuthenticationService} from "../../service/authentication/authentication.service";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../service/authentication/authentication.service';
 import {User} from '../../model/user';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.css']
+  styleUrls: ['./connexion.component.css'],
+  animations: [
+    trigger('formFalseValidationId', [
+      state('unchecked', style({})),
+      state('invalid', style({})),
+      transition('unchecked => invalid', animate(500, keyframes([
+        style({transform: 'translateX(-10%'}),
+        style({transform: 'translateX(10%'}),
+        style({transform: 'translateX(-10%'}),
+        style({transform: 'translateX(-10%'}),
+      ])))
+    ]),
+    trigger('formFalseValidationPassword', [
+      state('unchecked', style({})),
+      state('invalid', style({})),
+      transition('unchecked => invalid', animate(500, keyframes([
+        style({transform: 'translateX(-10%'}),
+        style({transform: 'translateX(10%'}),
+        style({transform: 'translateX(-10%'}),
+        style({transform: 'translateX(-10%'}),
+      ])))
+    ])]
 })
 export class ConnexionComponent implements OnInit {
   public user: User;
   erreurLogin = 'cover';
-  constructor(private router : Router,
-              private authentication : AuthenticationService) { }
-
+  formFalseValidationPassword = 'unchecked';
+  formFalseValidationId = 'unchecked';
+  animationRedId = false;
+  animationRedPassword = false;
+  constructor(private router: Router,
+              private authentication: AuthenticationService) {
+  }
   ngOnInit() {
     this.user = new User();
     this.erreurLogin = 'cover';
   }
-
-  login(){
+  setBackToUnchecked(){
+    this.formFalseValidationId = 'unchecked';
+    this.formFalseValidationPassword = 'unchecked';
+    this.animationRedId = false;
+    this.animationRedPassword = false;
+  }
+  login() {
     this.authentication.login(this.user);
-    if(this.user.email && this.user.password){
-      this.router.navigate(["/issueView"]);
-    } else{
+    if (this.user.email && this.user.password) {
+      this.router.navigate(['/issueView']);
+    } else {
+      if (!this.user.email) {
+        this.formFalseValidationId = 'invalid';
+        this.animationRedId = true;
+      }
+      if (!this.user.password) {
+        this.formFalseValidationPassword = 'invalid';
+        this.animationRedPassword = true;
+      }
       this.erreurLogin = 'display';
     }
   }
