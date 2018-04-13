f68fd3a8db8e38c60854e3d07a9fa7d37b0ba348
Gregoire Peltier <pg501842@etu.unice.fr>
2018-04-13 14:26:48 +0200

    SHB-47 Added the name route

--- back-end/common/models/person.js
+++ back-end/common/models/person.js
@@ -1,5 +1,30 @@
 'use strict';
 
 module.exports = function (Person) {
+  Person.getName = function (id, callback) {
+    Person.findById(id, function (err, instance) {
+      if (instance != null) {
+        var response = instance.username;
+        if (response == null) response = callback(null, "NoUserNameCONTACTADMIN_BLAME_THEOS");
+        callback(null, response);
+      }
+      else {
 
+        callback(null, null);
+      }
+    })
+  };
+  Person.remoteMethod(
+    'getName', {
+      http: {
+        path: '/name',
+        verb: 'get'
+      },
+      accepts: {arg: 'id', type: 'string', http: {source: 'query'}},
+      returns: {
+        arg: 'name',
+        type: 'string'
+      }
+    }
+  )
 };
