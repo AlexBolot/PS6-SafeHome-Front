0f0ec23e0388ca6c1b1b820260dc66dac5d9911a
Gregoire Peltier <pg501842@etu.unice.fr>
2018-06-14 13:48:50 +0200

    Added the node schedule queries to the schedule.js

--- back-end/common/models/schedule.js
+++ back-end/common/models/schedule.js
@@ -1,5 +1,62 @@
 'use strict';
-
+let nextActivation;
 module.exports = function (Schedule) {
+  const loopback = require('loopback');
+  // Schedule.on('attached',()=>console.log("bit"));
+  Schedule.on('attached', scheduleNextPing);
+  Schedule.on('set', scheduleNextPing);
 
+  const ds = loopback.createDataSource({
+    'baseURL': 'http://localhost:1880/',
+    connector: require('loopback-connector-rest'),
+    'operations': [
+      {
+        functions: {
+          'setActive': ['date', 'active', 'object'],
+        },
+        'template': {
+          'method': 'POST',
+          'url': 'http://localhost:1880/{object:string}/activate',
+          'body': {
+            'active': '{active:boolean}',
+            'date': '{date:string}',
+          }
+        }
+      },
+    ],
+  });
+  let nodeRed = ds.createModel('node');
+
+  function scheduleNextPing() {
+    clearTimeout(nextActivation);
+    let Type = Schedule.app.models["Domotic-item-type"];
+    Type.find(function (err, types) {
+      for (let type in types) {
+        Schedule.app.models["Domotic-item"].findOne({"where": {"typeId": types[type].id}},
+          function (err, item) {
+            Schedule.findOne(
+              {
+                "order": "start ASC",
+                "where": {"itemId": item.id},
+              },
+              function (err, schedule) {
+                setTimeout(() => activate(true, types[type].name, schedule.end), Date.now() - schedule.start.getTime());
+                setTimeout(() => activate(false, types[type].name, null), Date.now() - schedule.end.getTime());
+                activate(true, types[type].name, schedule.end)
+              }
+            )
+          });
+      }
+    });
+  };
+  Schedule.remoteMethod(
+    'nextPing', {
+      http: {path: '/next', verb: 'get'},
+      returns: {arg: 'result', type: 'string'},
+    });
+
+  function activate(value, item, end) {
+    nodeRed.setActive(end, value, item);
+  }
 };
+
