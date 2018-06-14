decf9f9c2cb73b9087b565ce78df22974bda1729
Mathieu Paillart <mathieu.paillart@etu.unice.fr>
2018-06-14 08:48:35 +0200

    SHB-89 : correction model schedule

--- smart-home-b/src/app/model/schedule.ts
+++ smart-home-b/src/app/model/schedule.ts
@@ -3,11 +3,13 @@ export class Schedule {
   start: Date;
   end: Date;
   domoticitemId: number;
+  auto:boolean;
 
-  constructor(idSchedule: number, start: Date, end: Date, domoticitemId: number) {
+  constructor(idSchedule: number, start: Date, end: Date, domoticitemId: number,auto:boolean) {
     this.id = idSchedule;
     this.start = start;
     this.end = end;
     this.domoticitemId = domoticitemId;
+    this.auto=auto;
   }
 }
